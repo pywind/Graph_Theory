31833ccba1d92c49fbc0c1b77216c344ed7b7a23
Vũ Phước <56511923+pywind@users.noreply.github.com>
2020-06-25 21:28:15 +0700

    Update DEMOBFS.cpp

--- Spanning_Tree/BFS/DEMOBFS.cpp
+++ Spanning_Tree/BFS/DEMOBFS.cpp
@@ -1,10 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include <queue>
+
 
 using namespace std;
 
-int D[50] = { 0 };
-int p[50] = { 0 };
+int D[50];
+int p[50];
+
 //void init(int W[][50], int& c) {
 //    cin >> c;
 //    for (int i = 0; i < c; ++i) {
@@ -16,55 +18,47 @@ int p[50] = { 0 };
 //}
 void init(int W[][50], int& n, int& m, int& s) {
     cin >> n >> m >> s;
-    // <++ n dinh m canh ++>
+    // <++ n vertex m edge ++>
     for (int i = 1; i <= m; ++i) {
         int u, v;
         cin >> u >> v;
         W[u][v] = 1;
         W[v][v] = 1;
+        D[i] = 0;
     }
 }
-void display(queue <int> a, int n) {
-    while (!a.empty()) {
-        cout << a.front() << ",";
-        a.pop();
-    }
-    cout << "  ";
-    for (int i = 1; i <= n; ++i) {
-        cout << D[i] << "," << p[i] << "  ";
-    }
-    cout << endl;
-}
+
 void BFS(int W[][50], int n, int m, int s) {
-    D[1] = 1;
-    p[1] = -1;
+    D[s] = 1;
+    p[s] = -1;
     queue <int> Q;
     Q.push(s);
     while (!Q.empty()) {
         int u = Q.front();
-        Q.pop();
         for (int v = 1; v <= n; ++v) {
-            if (W[u][v] > 0 && D[v] == 0) {
+            if ((W[u][v] > 0) && !D[v]) {
                 D[v] = 1;
                 p[v] = u;
                 Q.push(v);
             }
         }
-        display(Q, n);
+        Q.pop(); 
     }
 }
 void tracing(int n) {
-    if (n > 0) {
-        tracing(p[n]);
-        cout << n << " ";
+    for (int i = 1; i <= n; ++i) {
+        if (p[i] > 0)
+            cout << i << " -> " << p[i] << endl;
+        else 
+            cout << i << ": " << "Root" << endl;
     }
 }
 int main() {
-    int n , m, s;
+    int n, m, s;
     int W[50][50];
     init(W, n, m, s);
     BFS(W, n, m, s);
-    cout << "Cay khung: ";
+    cout << "Spanning tree " << endl;
     tracing(n);
 }
 
@@ -89,5 +83,6 @@ int main() {
 2 4
 2 6
 3 7
+5 6
+
 */
-//5 6
