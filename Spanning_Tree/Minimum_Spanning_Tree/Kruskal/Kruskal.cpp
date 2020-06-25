9111dcdc65c38812042f01414aeba3572e5e003c
Vũ Phước <56511923+pywind@users.noreply.github.com>
2020-06-25 07:56:12 +0700

    Update Kruskal.cpp

--- Spanning_Tree/Minimum_Spanning_Tree/Kruskal/Kruskal.cpp
+++ Spanning_Tree/Minimum_Spanning_Tree/Kruskal/Kruskal.cpp
@@ -7,7 +7,7 @@
 #define arris second
 using namespace std;
 
-// <--->
+// 
 typedef pair<int, int> VE;
 typedef pair<int, VE> node;
 // weight, <v, e> weight is first . 1st: v, 2nd: e 
@@ -19,62 +19,63 @@ int parent[2001];
 //
 
 void init() {
-    cin >> n >> m;
-    while (m--) {
-        int e, v, w;
-        cin >> v >> e >> w;
-        edge.push_back(node(w, VE(v, e)));
-    }
-    
+	cin >> n >> m;
+	while (m--) {
+		int e, v, w;
+		cin >> v >> e >> w;
+		edge.push_back(node(w, VE(v, e)));
+	}
+
 
 }
 void display() {
-    for (int i = 0; i <= n; ++i) {
-        cout << parent[i] << " ";
-    }
-    cout << endl;
+	for (int i = 0; i <= n; ++i) {
+		cout << parent[i] << " ";
+	}
+	cout << endl;
 }
-int findSet(int p) { 
-    if (parent[p] == p)
-        return p;
-    else
-        return parent[p] = findSet(parent[p]);
+int findSet(int p) {
+	if (parent[p] == p)
+		return p;
+	else
+		parent[p] = findSet(parent[p]);
+	return parent[p];
 }
-void join(int u, int v) { 
-    parent[findSet(u)] = findSet(v); 
+void Union(int u, int v) {
+	parent[findSet(u)] = findSet(v);
 }
 void makeSet() {
-    iota(parent, parent + n + 1, 0);
+	iota(parent, parent + n + 1, 0);
 }
 
 
 
 int main() {
-    // Reading data 
-    init();
-    // Create a set with only one vertex contain v
-    makeSet();
-    // Sort by first: weight
-    sort(edge.begin(), edge.end());
+	// Reading data 
+	init();
+	// Create a set with only one vertex contain v
+	makeSet();
+	// Sort by first: weight
+	sort(edge.begin(), edge.end());
 
-    //vector<node>::iterator index;
-    int mst_weight = 0;
-    display();
-    for (auto index = edge.begin(); index != edge.end(); index++) {
-        int u = index->arris.weight;
-        int v = index->arris.arris;
-        if (findSet(u) != findSet(v)) {
-            join(u, v);
-            mst_weight += index->weight;
-            A.push_back(index->arris);
-            display();
-        }
-    }
-    cout << mst_weight << endl;
-    /*sort(A.begin(), A.end());
-    for (auto i : A) {
-        cout << i.weight << " " << i.arris << endl;
-    }*/
+	//vector<node>::iterator index;
+	int mst_weight = 0;
+	display();
+	for (auto index = edge.begin(); index != edge.end(); index++) {
+		int u = index->arris.weight;
+		int v = index->arris.arris;
+		if (findSet(u) != findSet(v)) {
+			Union(u, v);
+			mst_weight += index->weight;
+			A.push_back(index->arris);
+			display();
+		}
+	}
+	cout << mst_weight << endl;
+	/*sort(A.begin(), A.end());
+	for (auto i : A) {
+		cout << i.weight << " " << i.arris << endl;
+	}*/
 }
 /*
 
